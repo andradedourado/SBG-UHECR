894e6499fddb7afd5c48eeadd7493ea06530c399
Luciana Andrade Dourado <luciana.dourado@gssi.it>
2025-03-07 12:16:10 +0100

    Plot photopion inelasticity

--- timescales/scripts/plot_photopion_inelasticity.py
+++ timescales/scripts/plot_photopion_inelasticity.py
@@ -1,36 +1,43 @@
 import matplotlib.pyplot as plt
 import numpy as np 
 
+plt.rcParams.update({'legend.fontsize': 'large',
+'legend.title_fontsize': 'large',
+'axes.labelsize': 'x-large',
+'axes.titlesize': 'xx-large',
+'xtick.labelsize': 'x-large',
+'ytick.labelsize': 'x-large'})
+
+FIGURES_DIR = "../figures"
+
 # ----------------------------------------------------------------------------------------------------
-def inelasticity(eps_prime):
+def inelasticity(x): 
 
     Y_inf = 0.47
-    xb = 6e9
+    xb = 6e9 # eV
     dlt = 0.33
     s = 0.15
 
-    return 
+    return Y_inf * (x / xb)**dlt / (1 + (x / xb)**(dlt / s))**s
 
 # ----------------------------------------------------------------------------------------------------
 def plot_photopion_inelasticity():
 
+    x = np.logspace(8, 14, num = 100) # x = eps_prime
+
+    plt.plot(x, inelasticity(x))
+    plt.xscale('log')
+    plt.xlim([1e8, 4e13])
+    plt.ylim([0, 0.7])
+    plt.xlabel(r'Photon energy$\: \rm [eV]$')
+    plt.ylabel(r'Inelasticity')
+    plt.savefig(f"{FIGURES_DIR}/inelasticity.pdf", bbox_inches = 'tight')
+    plt.savefig(f"{FIGURES_DIR}/inelasticity.png", bbox_inches = 'tight', dpi = 300)
     plt.show()
 
 # ----------------------------------------------------------------------------------------------------
-if __name__ is '__main__':
+if __name__ == '__main__':
 
     plot_photopion_inelasticity()
 
 # ----------------------------------------------------------------------------------------------------
-
-
-
-
-# double inelasticity(double epsPrime) {
-#   static const double Y0 = 0.47;
-#   static const double b = 6e9 * SI::eV;
-#   static const double d = 0.33;
-#   static const double s = 0.15;
-#   const auto x = epsPrime / b;
-#   return Y0 * std::pow(x, d) / std::pow(1. + std::pow(x, d / s), s);
-# }
