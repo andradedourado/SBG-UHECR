6b5a1bf8a80ae320741fd4cd002e0ab885c50fac
Luciana Andrade Dourado <luciana.dourado@gssi.it>
2025-03-21 15:41:22 +0100

    Add interpolation for η values in KA08 implementation

--- sec-spec/scripts/compute_KA08_spectrum.py
+++ sec-spec/scripts/compute_KA08_spectrum.py
@@ -1,4 +1,5 @@
 from scipy.integrate import trapz, simps
+from scipy.interpolate import interp1d
 import numpy as np
 
 RESULTS_DIR = "../results"
@@ -10,10 +11,10 @@ h = 4.135667696e-15 # eV.s
 kB = 8.6173303e-5   # eV/K
 mp = 0.9383e9       # eV 
 
-eta0 = 0.313 
+eta0 = 0.313
 
 # ----------------------------------------------------------------------------------------------------
-def photon_density(eps, T = 2.7): # 1 / cm^-3 / eV 
+def photon_density_CMB(eps, T = 2.7): # 1 / cm^-3 / eV 
 
     return 8 * np.pi * eps**2 / (h * c)**3 / (np.exp(eps / (kB * T)) - 1)
 
@@ -33,14 +34,19 @@ def write_mono_spectrum(l, Ep): # Equation (42)
     phi_l = np.loadtxt(f"{RESULTS_DIR}/KA08_phi_{l}.dat")[:,1:] # cm^3 / s
     spec = [] # 1 / s
 
-    eps = eta * mp**2 / (4 * Ep)
-
     for ix in range(len(xs)):
-        integrand = photon_density(eps) * phi_l[ix,:]
+
+        f = interp1d(eta, phi_l[ix,:], kind = 'linear', bounds_error = False, fill_value = 'extrapolate')
+        eta_new = np.logspace(np.log10(eta[0]), np.log10(eta[-1]), num = 100)
+        phi_l_new = f(eta_new)
+
+        eps = eta_new * mp**2 / (4 * Ep)
+        integrand = photon_density_CMB(eps) * phi_l_new
+
         if simps(integrand, eps) < 0:
             spec.append(trapz(integrand, eps))
         else:
-            spec.append(simps(integrand, eps))
+            spec.append(simps(integrand, eps)) 
 
     spec = np.array(spec)
     np.savetxt(f"{RESULTS_DIR}/KA08_mono_spectrum_CMB_Ep{int(np.log10(Ep)):02d}_{l}.dat", np.column_stack((xs, spec)), fmt = "%.15e")
