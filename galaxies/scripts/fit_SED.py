5d4e9c412f00bd065d47109fc5c47a651f9820bf
Luciana Andrade Dourado <luciana.dourado@gssi.it>
2025-05-05 16:34:41 +0200

    Fit graybody distributions to the SEDs of galaxies from the Lunardini et al. (2019) catalog

--- galaxies/scripts/fit_SED.py
+++ galaxies/scripts/fit_SED.py
@@ -5,12 +5,13 @@ import pandas as pd
 DATA_DIR = "../data"
 RESULTS_DIR = "../results"
 
+gal_data = np.genfromtxt('starburst_galaxies.dat', dtype = None, encoding = None)
+
 erg_to_eV = 6.242e11 
 eV_to_GeV = 1.e-9    
 Jy_to_CGS = 1e-23 # erg·cm^-2·s^-1·Hz^-1
 pc_to_cm = 3.0857e18 
 
-D = 3.7e6 * pc_to_cm # cm  
 R = 225 * pc_to_cm   # cm 
 
 c = 3.e10           # cm/s
@@ -18,55 +19,98 @@ h = 4.135667696e-15 # eV·s
 kB = 8.6173303e-5   # eV/K
 
 # ----------------------------------------------------------------------------------------------------
+def get_initial_guess(photon_spectrum):
+
+    if photon_spectrum == 'IR':
+        return [0.1, 3.5e-3 / kB, 1.0]
+    elif photon_spectrum == 'OPT':
+        return [1e-09, 332.5e-3 / kB, 1.0]
+
+# ----------------------------------------------------------------------------------------------------
 def load_SED_measurements(galaxy):
 
-    data = pd.read_csv(f"{DATA_DIR}/data_{galaxy}.csv")
+    if galaxy in ('NGC4038', 'NGC4039'):
+        igal = 36
+        data = pd.read_csv(f"{DATA_DIR}/data_{igal}_{galaxy}.csv")
+    
+    else:
+        for igal in range(len(gal_data)):
+            if galaxy == gal_data[igal][2]:
+                break
+        igal = igal + 2
+        data = pd.read_csv(f"{DATA_DIR}/data_{igal:02d}_{galaxy}.csv")
 
     frequency = data["Frequency (Hz)"].to_numpy() 
     energy = frequency * h
     flux_density = data["Flux Density"].to_numpy() # Jy = 10^-26 W·m^-2·Hz^-1
     flux_density = flux_density * Jy_to_CGS * erg_to_eV * eV_to_GeV
 
-    return energy, frequency*flux_density
+    return energy, frequency*flux_density, igal 
 
 # ----------------------------------------------------------------------------------------------------
-def photon_density(E, Cdil, E0, T, sgm): # 1 / eV / cm^3
+def photon_density(E, Cdil, T, sgm): # 1 / eV / cm^3
 
-    return Cdil * 8 * np.pi * E**2 / (h * c)**3 / (np.exp(E / (kB * T)) - 1) * (E / E0)**sgm
-
-# ----------------------------------------------------------------------------------------------------
-def SED(E, Cdil, E0, T, sgm): 
-
-    return 4/9 * (R/D)**2 * c * photon_density(E, Cdil, E0, T, sgm) * E**2 * eV_to_GeV
+    return Cdil * 8 * np.pi * E**2 / (h * c)**3 / (np.exp(E / (kB * T)) - 1) * (E / (kB * T))**sgm
 
 # ----------------------------------------------------------------------------------------------------
-def fit_SED(galaxy):
+def SED(E, Cdil, T, sgm, galaxy): 
 
-    x_data, y_data = load_SED_measurements(galaxy)
+    if galaxy in ('NGC4038', 'NGC4039'):
+        D = 23.7e6 * pc_to_cm
+    else:
+        D = float(gal_data[[g[2] for g in gal_data].index(galaxy)][8]) * 1e6 * pc_to_cm
 
-    mask_energy = (x_data >= 1e-4) & (x_data <= 1e-1)
+    return 4/9 * (R/D)**2 * c * photon_density(E, Cdil, T, sgm) * E**2 * eV_to_GeV
+
+# ----------------------------------------------------------------------------------------------------
+def fit_SED(galaxy, photon_spectrum):
+
+    x_data, y_data, igal = load_SED_measurements(galaxy)
+
+    if photon_spectrum == 'IR':
+        mask_energy = (x_data >= 1e-4) & (x_data <= 1e-1)
+        initial_guess = get_initial_guess(photon_spectrum)
+        fit_func = lambda E, Cdil, T, sgm: SED(E, Cdil, T, sgm, galaxy)
+        bounds = ([0, 0, 0], [np.inf, np.inf, np.inf])
+        p0 = initial_guess
+
+    elif photon_spectrum == 'OPT':
+        mask_energy = (x_data >= 1e-2) & (x_data <= 1e2)
+        initial_guess = get_initial_guess(photon_spectrum)[:2]
+        fit_func = lambda E, Cdil, T: SED(E, Cdil, T, 0, galaxy)
+        bounds = ([0, 0], [np.inf, np.inf])
+        p0 = initial_guess
+
     mask_finite = np.isfinite(x_data) & np.isfinite(y_data)
     mask = mask_energy & mask_finite
 
     x_data = x_data[mask]
     y_data = y_data[mask]
-    
-    initial_guess = [0.1, 3.5e-3, 3.5e-3 / kB, 1.0]
 
-    lower_bounds = [0, 0, 0, 0]
-    upper_bounds = [np.inf, np.inf, np.inf, np.inf]
+    popt, _ = curve_fit(fit_func, x_data, y_data, p0 = p0, bounds = bounds, method = 'dogbox')
 
-    return curve_fit(SED, x_data, y_data, p0 = initial_guess, bounds = (lower_bounds, upper_bounds))
+    if photon_spectrum == 'OPT':
+        popt = np.append(popt, 0.0)
+
+    return popt, igal
 
 # ----------------------------------------------------------------------------------------------------
-def write_fitted_parameters(galaxy):
+def write_fitted_parameters(galaxy, photon_spectrum):
 
-    header = 'Cdil' + '\t' + 'E0' + '\t' + 'T' + '\t' + 'sgm'
-    np.savetxt(f"{RESULTS_DIR}/fitted_params_{galaxy}.dat", np.column_stack((fit_SED(galaxy)[0])), fmt = "%.15e", delimiter = "\t", header = header)
+    header = 'Cdil' + '\t' + 'T' + '\t' + 'sgm'
+
+    if galaxy == 'NGC4038/9':
+        np.savetxt(f"{RESULTS_DIR}/fitted_params_36_NGC4038_{photon_spectrum}.dat", fit_SED('NGC4038', photon_spectrum)[0].reshape(1, -1), fmt = "%.15e", delimiter = "\t", header = header)
+        np.savetxt(f"{RESULTS_DIR}/fitted_params_36_NGC4039_{photon_spectrum}.dat", fit_SED('NGC4039', photon_spectrum)[0].reshape(1, -1), fmt = "%.15e", delimiter = "\t", header = header)
+    else:
+        igal = fit_SED(galaxy, photon_spectrum)[1]
+        np.savetxt(f"{RESULTS_DIR}/fitted_params_{igal:02d}_{galaxy}_{photon_spectrum}.dat", fit_SED(galaxy, photon_spectrum)[0].reshape(1, -1), fmt = "%.15e", delimiter = "\t", header = header)
 
 # ----------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
-    
-    write_fitted_parameters('NGC253')
+
+    for igal in range(len(gal_data)):
+        write_fitted_parameters(gal_data[igal][2], 'IR')
+        write_fitted_parameters(gal_data[igal][2], 'OPT')
 
 # ----------------------------------------------------------------------------------------------------
--- galaxies/scripts/plot_fitted_SED.py
+++ galaxies/scripts/plot_fitted_SED.py
@@ -1,3 +1,4 @@
+from matplotlib.offsetbox import AnchoredText
 import matplotlib.pyplot as plt 
 import numpy as np
 import pandas as pd
@@ -13,12 +14,13 @@ DATA_DIR = "../data"
 FIGURES_DIR = "../figures"
 RESULTS_DIR =  "../results"
 
+gal_data = np.genfromtxt('starburst_galaxies.dat', dtype = None, encoding = None)
+
 erg_to_eV = 6.242e11
 eV_to_GeV = 1.e-9
 Jy_to_CGS = 1e-23 # erg·cm^-2·s^-1·Hz^-1    
 pc_to_cm = 3.0857e18 
 
-D = 3.7e6 * pc_to_cm # cm  
 R = 225 * pc_to_cm  # cm 
 
 c = 3.e10           # cm/s
@@ -26,19 +28,42 @@ h = 4.135667696e-15 # eV·s
 kB = 8.6173303e-5   # eV/K
 
 # ----------------------------------------------------------------------------------------------------
-def photon_density(E, Cdil, E0, T, sgm): # 1 / eV / cm^3
-
-    return Cdil * 8 * np.pi * E**2 / (h * c)**3 / (np.exp(E / (kB * T)) - 1) * (E / E0)**sgm
+def get_spectrum_color(photon_spectrum):
+    
+    if photon_spectrum == 'IR':
+        return '#FF5555'
+    
+    elif photon_spectrum == 'OPT':
+        return '#5555FF'
 
 # ----------------------------------------------------------------------------------------------------
-def SED(E, Cdil, E0, T, sgm): 
+def photon_density(E, Cdil, T, sgm): # 1 / eV / cm^3
 
-    return 4/9 * (R/D)**2 * c * photon_density(E, Cdil, E0, T, sgm) * E**2 * eV_to_GeV
+    return Cdil * 8 * np.pi * E**2 / (h * c)**3 / (np.exp(E / (kB * T)) - 1) * (E / (kB * T))**sgm
+
+# ----------------------------------------------------------------------------------------------------
+def SED(E, Cdil, T, sgm, galaxy): 
+
+    if galaxy in ('NGC4038', 'NGC4039'):
+        D = 23.7e6 * pc_to_cm
+    else:
+        D = float(gal_data[[g[2] for g in gal_data].index(galaxy)][8]) * 1e6 * pc_to_cm
+
+    return 4/9 * (R/D)**2 * c * photon_density(E, Cdil, T, sgm) * E**2 * eV_to_GeV
 
 # ----------------------------------------------------------------------------------------------------
 def plot_photon_spectrum_measurements(galaxy):
 
-    data = pd.read_csv(f"{DATA_DIR}/data_{galaxy}.csv")
+    if galaxy in ('NGC4038', 'NGC4039'):
+        igal = 36
+        data = pd.read_csv(f"{DATA_DIR}/data_{igal}_{galaxy}.csv")
+    
+    else:
+        for igal in range(len(gal_data)):
+            if galaxy == gal_data[igal][2]:
+                break
+        igal = igal + 2
+        data = pd.read_csv(f"{DATA_DIR}/data_{igal:02d}_{galaxy}.csv")
 
     frequency = data["Frequency (Hz)"].to_numpy() 
     energy = frequency * h
@@ -47,25 +72,25 @@ def plot_photon_spectrum_measurements(galaxy):
 
     plt.scatter(energy, frequency*flux_density, s = 25, marker = 'o', color = 'gray', edgecolor = 'black')
 
+    return igal
+
 # ----------------------------------------------------------------------------------------------------
-def plot_fitted_SED_IR(galaxy):
+def plot_fitted_SED(galaxy):
 
-    E = np.logspace(-4, -1, num = 100)
+    E_IR = np.logspace(-4, -1, num = 100)
+    E_OPT = np.logspace(-2, 1, num = 100)
 
-    tab10 = plt.get_cmap('tab10')
-    tab10_colors = [tab10(i) for i in range(10)]
+    igal = plot_photon_spectrum_measurements(galaxy)
 
-    plot_photon_spectrum_measurements(galaxy)
+    for photon_spectrum in ('IR', 'OPT'):
+        if photon_spectrum == 'IR':
+            E = E_IR
+        elif photon_spectrum == 'OPT':
+            E = E_OPT
+        fitted_params = np.loadtxt(f"{RESULTS_DIR}/fitted_params_{igal:02d}_{galaxy}_{photon_spectrum}.dat")
+        plt.plot(E, SED(E, fitted_params[0], fitted_params[1], fitted_params[2], galaxy), color = get_spectrum_color(photon_spectrum), label = f'{photon_spectrum}')
 
-    fitted_params = np.loadtxt(f"{RESULTS_DIR}/fitted_params_{galaxy}.dat")
-    plt.plot(E, SED(E, fitted_params[0], fitted_params[1], fitted_params[2], fitted_params[3]), color = tab10_colors[3], label = 'LAD (GB)')
-    
-    data = np.loadtxt(f"../../timescales/results/photon_spectrum_IR.dat")
-    E, flux = data[:,0], data[:,1]  
-    plt.plot(E * erg_to_eV, E**2 * flux * erg_to_eV * eV_to_GeV, color = tab10_colors[1], label = 'LAD (BB)')
-
-    data = np.loadtxt(f"../../timescales/references/SED_Condorelli_IR.dat")
-    plt.plot(data[:,0], data[:,1], color = tab10_colors[8], label = 'Condo+23')
+    plt.gca().add_artist(AnchoredText(f'{galaxy}', loc = 'upper left', frameon = False, prop = {'fontsize': 'x-large'}))
 
     plt.xscale('log')
     plt.yscale('log')
@@ -73,14 +98,19 @@ def plot_fitted_SED_IR(galaxy):
     plt.ylim([1.e-10, 2.e-4])
     plt.xlabel(r'Energy$\: \rm [eV]$')
     plt.ylabel(r'$E^2 F(E) \: \rm [GeV \, cm^{-2} \, s^{-1}]$')
-    plt.legend()
-    plt.savefig(f"{FIGURES_DIR}/fitted_SED_IR.pdf", bbox_inches = 'tight')
-    plt.savefig(f"{FIGURES_DIR}/fitted_SED_IR.png", bbox_inches = 'tight', dpi = 300)
+    plt.legend(loc = 'lower left')
+    plt.savefig(f"{FIGURES_DIR}/fitted_SED_{igal:02d}_{galaxy}.pdf", bbox_inches = 'tight')
+    plt.savefig(f"{FIGURES_DIR}/fitted_SED_{igal:02d}_{galaxy}.png", bbox_inches = 'tight', dpi = 300)
     plt.show()
 
 # ----------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
-    
-    plot_fitted_SED_IR('NGC253')
+
+    for galaxy in gal_data:
+        if galaxy[2] == 'NGC4038/9':
+            plot_fitted_SED('NGC4038')
+            plot_fitted_SED('NGC4039')
+        else:
+            plot_fitted_SED(galaxy[2])
 
 # ----------------------------------------------------------------------------------------------------
