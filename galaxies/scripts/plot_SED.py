e10c8cca8a54dda7494be1964f948327e7ac29f7
Luciana Andrade Dourado <luciana.dourado@gssi.it>
2025-05-05 12:16:54 +0200

    Rename SED plot files

--- galaxies/scripts/plot_SED.py
+++ galaxies/scripts/plot_SED.py
@@ -25,26 +25,28 @@ h = 4.135667696e-15 # eV·s
 # ----------------------------------------------------------------------------------------------------
 def load_SED_measurements(galaxy):
 
-    if galaxy in ('NGC4038', 'NGC4039'):        
-        data = pd.read_csv(f"{DATA_DIR}/data_36_{galaxy}.csv")
+    if galaxy in ('NGC4038', 'NGC4039'):
+        igal = 36        
+        data = pd.read_csv(f"{DATA_DIR}/data_{igal}_{galaxy}.csv")
     
     else:
         for igal in range(len(gal_data)):
             if galaxy == gal_data[igal][2]:
                 break
-        data = pd.read_csv(f"{DATA_DIR}/data_{(igal + 2):02d}_{galaxy}.csv")
+        igal = igal + 2
+        data = pd.read_csv(f"{DATA_DIR}/data_{igal:02d}_{galaxy}.csv")
 
     frequency = data["Frequency (Hz)"].to_numpy() 
     energy = frequency * h
     flux_density = data["Flux Density"].to_numpy() # Jy = 10^-26 W·m^-2·Hz^-1
     flux_density = flux_density * Jy_to_CGS * erg_to_eV * eV_to_GeV
 
-    return energy, frequency*flux_density
+    return energy, frequency*flux_density, igal
 
 # ----------------------------------------------------------------------------------------------------
 def plot_SED(galaxy):
 
-    E, SED = load_SED_measurements(galaxy)
+    E, SED, igal = load_SED_measurements(galaxy)
 
     plt.scatter(E, SED, s = 25, marker = 'o', color = 'gray', edgecolor = 'black', label = galaxy)
 
@@ -54,8 +56,8 @@ def plot_SED(galaxy):
     plt.yscale('log')
     plt.xlabel(r'Energy$\: \rm [eV]$')
     plt.ylabel(r'$E^2 F(E) \: \rm [GeV \, cm^{-2} \, s^{-1}]$') 
-    plt.savefig(f"{FIGURES_DIR}/SED_{galaxy}.pdf", bbox_inches = 'tight')
-    plt.savefig(f"{FIGURES_DIR}/SED_{galaxy}.png", bbox_inches = 'tight', dpi = 300)    
+    plt.savefig(f"{FIGURES_DIR}/SED_{igal}_{galaxy}.pdf", bbox_inches = 'tight')
+    plt.savefig(f"{FIGURES_DIR}/SED_{igal}_{galaxy}.png", bbox_inches = 'tight', dpi = 300)    
     plt.show()
 
 # ----------------------------------------------------------------------------------------------------
