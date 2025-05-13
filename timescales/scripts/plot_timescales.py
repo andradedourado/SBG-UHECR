b85bb7d4b25bcbeefce9bb3d469aee3c81ac9059
Luciana Andrade Dourado <luciana.dourado@gssi.it>
2025-05-13 15:07:47 +0200

    Add plot showing only proton timescales

--- timescales/scripts/plot_timescales.py
+++ timescales/scripts/plot_timescales.py
@@ -67,8 +67,29 @@ def plot_timescales():
     plt.show()
 
 # ----------------------------------------------------------------------------------------------------
+def plot_proton_timescales():
+
+    data_advection = np.loadtxt(f"{RESULTS_DIR}/timescales_advection.dat") 
+    data_diff_1H = np.loadtxt(f"{RESULTS_DIR}/timescales_diff_1H.dat")
+    data_spal_1H = np.loadtxt(f"{RESULTS_DIR}/timescales_spal_1H.dat")
+    data_photohadronic_1H = compute_photohadronic_timescales(1)
+
+    plt.plot(np.log10(data_advection[:,0] * 1.e18), data_advection[:,1], label = 'Advection')
+    plt.plot(np.log10(data_diff_1H[:,0] * 1.e18), data_diff_1H[:,1], label = f'Diffusion')
+    plt.plot(np.log10(data_spal_1H[:,0] * 1.e18), data_spal_1H[:,1], label = f'Hadronic')
+    plt.plot(np.log10(data_photohadronic_1H[0]), data_photohadronic_1H[1], label = f'Photohadronic')
+    plt.yscale('log')
+    plt.ylim([1.e2, 1.e7])
+    plt.xlabel(r'$\log_{10}{({\rm Energy}/{\rm eV})}$')
+    plt.ylabel(r'Timescales$\: \rm [yr]$')
+    plt.legend(bbox_to_anchor = (0., 1.02, 1., .102), loc = 'lower left', ncols = 2, mode = "expand", borderaxespad = 0.)
+    plt.savefig(f"{FIGURES_DIR}/proton_timescales.pdf", bbox_inches = 'tight')
+    plt.savefig(f"{FIGURES_DIR}/proton_timescales.png", bbox_inches = 'tight', dpi = 300)
+    plt.show()
+
+# ----------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
 
-    plot_timescales()
+    plot_proton_timescales()
 
 # ----------------------------------------------------------------------------------------------------
